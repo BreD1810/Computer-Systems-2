203be2ac003ea1e3cc9f4569bb16ab0ccf3ea5e1
BreD1810 <bradleybredgarrod@gmail.com>
2019-03-06 22:00:48 +0000

    Added .h and .c files with some basic methods. Still need to deside on the values of constants.

---
+++ tasks/task4/fileSystem.c
@@ -0,0 +1,41 @@
+#include "fileSystem.h"
+
+void init_eepromfs()
+{
+
+}
+
+void open_for_write(uint8_t fileName)
+{
+
+}
+
+void open_for_append(uint8_t fileName)
+{
+
+}
+
+void open_for_read(uint8_t fileName)
+{
+
+}
+
+void close(uint8_t fileName)
+{
+
+}
+
+void write(uint8_t fileName, uint8_t* buffer, uint16_t noOfBytes)
+{
+
+}
+
+uint16_t read(uint8_t fileName, uint8_t* buffer, uint16_t noOfBytes)
+{
+
+}
+
+void delete_file(uint8_t fileName)
+{
+    
+}
---
+++ tasks/task4/fileSystem.h
@@ -0,0 +1,34 @@
+#ifndef _EEPROMFS_
+#define _EEPROMFS_
+
+#include <avr/eeprom.h>
+#include <stdint.h>
+
+#define BLOCK_SIZE 64
+#define MAX_FILES 
+#define EEPROM_LENGTH 
+#define EEPROM_START 
+#define TOTALBLOCKS = EEPROM_LENGTH/BLOCK_SIZE
+
+typedef struct entry
+{
+    uint8_t startBlock;
+    uint16_t fileLength;
+    uint16_t currentPosition;
+} directoryEntry;
+
+typedef struct fs_meta
+{
+    directoryEntry dir[MAX_FILES];
+} metaData;
+
+void init_eepromfs();
+void open_for_write(uint8_t fileName);
+void open_for_append(uint8_t fileName);
+void open_for_read(uint8_t fileName);
+void close(uint8_t fileName);
+void write(uint8_t fileName, uint8_t* buffer, uint16_t noOfBytes);
+uint16_t read(uint8_t fileName, uint8_t* buffer, uint16_t noOfBytes);
+void delete_file(uint8_t fileName);
+
+#endif //_EEPROMFS_
